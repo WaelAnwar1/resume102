65d533ac8bd64cfb2d7f37adf8fe8fcca248aa08
waelanwar1 <waelanwar.etx@gmail.com>
2024-11-03 16:05:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+/*************  Typing Animation ******************* */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "IT Consultant", "ERP Projects Manager"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
---
+++ res.md
@@ -0,0 +1,4 @@
+Resources::
+https://www.youtube.com/watch?v=zJE-ze4TfXc&list=PLrKudo5h_b8CKGtcPWIYtWmbTokv8QhlY&index=1
+
+part# 1 :: 2h : 44min
